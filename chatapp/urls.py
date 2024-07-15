d567782ac9a6a2570f37975a6aea68d1b03ca5bc
qchen <ichenq@outlook.com>
2024-07-15 14:43:48 +0800

    cache control

--- chatapp/urls.py
+++ chatapp/urls.py
@@ -7,7 +7,6 @@ app_name = 'chatapp'
 urlpatterns = [
     path('login', views.login, name='login'),
     path('logout', views.logout, name='logout'),
-    path('chat-process', views.ChatView.as_view(http_method_names=['post']), name='chat-process'),
-    path('chat', views.ChatView.as_view(), name='chat'),
+    path('chat-process', never_cache(views.ChatView.as_view(http_method_names=['post'])), name='chat-process'),
     path('chat-stream', never_cache(views.ChatStreamView.as_view()), name='chat-stream'),
 ]
